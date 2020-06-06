2c0f186368f310f8d560755d65564336d8ebb3ff
jony.huang <jony.huang@corp.to8to.com>
2020-06-06 18:06:43 +0800

    【新增】字母异位词分组-更优

--- src/main/java/com/guopeng/algorithm/LetterEccentricGrouping.java
+++ src/main/java/com/guopeng/algorithm/LetterEccentricGrouping.java
@@ -4,6 +4,8 @@ package com.guopeng.algorithm;
 import java.util.*;
 
 /**
+ *
+ * 更优方法：https://www.jianshu.com/p/b50d790e9f76
  * 字母异位词分组
  *
  * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
@@ -29,14 +31,19 @@ public class LetterEccentricGrouping {
 
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> result = new ArrayList<>();
-        Map<String, List<String>> map = new HashMap<>();
+        Map<Long, List<String>> map = new HashMap<>();
         //1.遍历strs
         for (int i = 0; i < strs.length; i++) {
             String strings = strs[i];
             char[] chars = strings.toCharArray();
             //2.重排序
-            Arrays.sort(chars);
-            String s2 =  new String(chars);
+//            Arrays.sort(chars);
+            Long s2 = 1L;
+            for (int j = 0; j < chars.length; j++) {
+                int c = chars[j]-'a'+1;
+                s2 = s2*c;
+            }
+//            String s2 =  new String(chars);
             if(map.containsKey(s2)){
                 map.get(s2).add(strings);
             }else {
@@ -47,7 +54,7 @@ public class LetterEccentricGrouping {
         }
 
         //3.遍历map，求结果合并
-        for (Map.Entry<String, List<String>> integerListEntry : map.entrySet()) {
+        for (Map.Entry<Long, List<String>> integerListEntry : map.entrySet()) {
             result.add(integerListEntry.getValue());
         }
         return result;
