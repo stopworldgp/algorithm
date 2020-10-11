073b1226437b4ae3ba8c028be4c46b9657fa5367
stopworldgp <guopeng_huang@163.com>
2020-10-11 20:11:32 +0800

    【新增】股票的最大利润

---
+++ src/test/java/com/guopeng/algorithm/company/bytedance/MaxProfitTest.java
@@ -0,0 +1,32 @@
+package com.guopeng.algorithm.company.bytedance;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author theone
+ * @Date 2020/10/11 20:03
+ * @Version 1.0
+ */
+public class MaxProfitTest {
+
+    MaxProfit maxProfit = new MaxProfit();
+
+    @Test
+    public void findMaxProfit() {
+        int[] array = {7,1,5,3,6,4};
+        int expect = 5;
+        int actaul = maxProfit.findMaxProfit(array);
+        Assert.assertEquals(expect,actaul);
+    }
+
+    @Test
+    public void findMaxProfit1() {
+        int[] array = {7,6,4,3,1};
+        int expect = 0;
+        int actaul = maxProfit.findMaxProfit(array);
+        Assert.assertEquals(expect,actaul);
+    }
+}
