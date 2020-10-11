7aa47f61c0ad91504641d51730189f6faaabb0f0
stopworldgp <guopeng_huang@163.com>
2020-10-11 18:55:52 +0800

    【新增】积水量问题

---
+++ src/main/java/com/guopeng/algorithm/company/bytedance/StagnantWater.java
@@ -0,0 +1,27 @@
+package com.guopeng.algorithm.company.bytedance;
+
+/**
+ *
+ * 描述：
+ * 凹凸不平的地面每当下雨的时候总会积水。假设地面是一维的，每一块宽度都为1，高度是非负整数，那么可以用一个数组来表达一块地面。例如[0,1,0,2,1,0,1,3,2,1,2,1]可以用来表示下图地面：
+ * 来源：http://blog.sina.com.cn/s/blog_3fba24680102xi2t.html
+ *
+ * 思路：
+ * https://leetcode-cn.com/problems/volume-of-histogram-lcci/solution/shuang-zhi-zhen-zhi-fang-tu-de-shui-liang-by-realz/
+ * @Author theone
+ * @Date 2020/10/11 18:12
+ * @Version 1.0
+ */
+public class StagnantWater {
+
+    public int amount(int[] array){
+        //1.按层计算
+
+        for (int i = 0; i < array.length; i++) {
+
+        }
+        
+        return 1;
+    }
+
+}
