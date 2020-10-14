0ce8a2be453217158b62a89261223c1d87a71782
jony.huang <jony.huang@corp.to8to.com>
2020-10-14 20:49:36 +0800

    【新增】LRU缓存

--- src/main/java/com/guopeng/algorithm/company/bytedance/LRUCache.java
+++ src/main/java/com/guopeng/algorithm/company/bytedance/LRUCache.java
@@ -8,31 +8,34 @@ import java.util.Map;
  * 题目： LRU缓存
  * 解题思路：
  * 1.直接利用LinkedHashMap
- * 2.双链表加hashmap(属性 hashmap，head,tail 都给予赋值()，写一个moveToTail方法，每次访问都把节点放在最后)
+ * 2.双链表加hashmap(属性 hashmap，head,tail 都给予赋值()，写一个moveToTail方法，每次访问都把节点放在最后,)
  *
  * @author jony.huang
  * @date 2020/10/14 19:52
  */
 public class LRUCache {
 
-    int capacity = 0;
-    private Map<Integer, Integer> linkedHashMap;
+    int capacity;
+    Map<Integer, Integer> map;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        int size = capacity + capacity / 3;
-        linkedHashMap = new LinkedHashMap(size, 0.75f, true);
+        map = new LinkedHashMap<>((int)((float)capacity / 0.75f + 1.0f), 0.75f, true);
     }
 
-    public int get(int key) {
-        return linkedHashMap.get(key);
+    public int get(int key)
+    {
+        Integer value = map.get(key);
+        if (value == null)
+            return -1;
+        return value;
     }
+
 
     public void put(int key, int value) {
-        if (linkedHashMap.size() >= capacity) {
-            linkedHashMap.remove(linkedHashMap.entrySet().iterator().next().getKey());
-        } else {
-            linkedHashMap.put(key, value);
+        map.put(key, value);
+        if (map.size() > capacity) {
+            map.remove(map.entrySet().iterator().next().getKey());
         }
     }
 }
