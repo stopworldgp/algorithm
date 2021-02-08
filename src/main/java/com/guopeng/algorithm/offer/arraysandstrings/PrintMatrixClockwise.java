2db4f9b33cf8b4590d76ac6aea224cf05d66cefe
stopworldgp <guopeng_huang@163.com>
2021-02-08 22:26:06 +0800

    【新增】数组中出现次数超过一半的数字

--- src/main/java/com/guopeng/algorithm/offer/arraysandstrings/PrintMatrixClockwise.java
+++ src/main/java/com/guopeng/algorithm/offer/arraysandstrings/PrintMatrixClockwise.java
@@ -15,7 +15,9 @@ package com.guopeng.algorithm.offer.arraysandstrings;
  * 注意事项：
  * 从左往右，从下到上 需要加个校验   if (left < right && top < bottom)  防止剩下一行时，从左往右加了一遍后又从右往左来一遍(靠 top < bottom控制)
  * 同理 剩一列
- *
+ * 注意事项:
+ *  //2.3 从上往下    遍历的时候 每个边都有交集的地方需要注意
+ *  for (int row = top+1; row <= bottom; row++) {
  * @author jony.huang
  * @date 2021/2/8 16:40
  */
@@ -28,36 +30,39 @@ public class PrintMatrixClockwise {
             return new int[0];
         }
         //1. 初始化结果数组
-        int rowMax = matrix.length;
-        int colMax = matrix[0].length;
-        int[] result = new int[rowMax * colMax];
-        //2.1 层循环
         int index = 0;
         int top = 0;
         int left = 0;
-        int right = colMax;
-        int bottom = rowMax;
-        //2.2 从左往右
-        for (int col = left; col < right; col++) {
-            result[index++] = matrix[top][col];
-        }
-        //2.3 从上往下
-        for (int row = top; row < bottom; row++) {
-            result[index++] = matrix[row][right];
-        }
-        //2.4 校验剩下一行或一列 避免重复添加，从左往右 又再同一行从右往左
-        if (left < right && top < bottom) {
-            //2.5 从右到左
-            for (int col = right-1; col >=left; col--) {
-                result[index++] = matrix[bottom][col];
+        int right = matrix[0].length-1;
+        int bottom = matrix.length-1;
+        int[] result = new int[ matrix[0].length *  matrix.length];
+        //2.1 层循环
+        while (left <= right && top <= bottom) {
+            //2.2 从左往右
+            for (int col = left; col <= right; col++) {
+                result[index++] = matrix[top][col];
             }
-            //2.6 从下到上
-            for (int row = bottom-1; row >=top; row--) {
-                result[index++] = matrix[row][left];
+            //2.3 从上往下
+            for (int row = top+1; row <= bottom; row++) {
+                result[index++] = matrix[row][right];
             }
+            //2.4 校验剩下一行或一列 避免重复添加，从左往右 又再同一行从右往左
+            if (left < right && top < bottom) {
+                //2.5 从右到左
+                for (int col = right - 1; col >= left; col--) {
+                    result[index++] = matrix[bottom][col];
+                }
+                //2.6 从下到上
+                for (int row = bottom-1; row > top; row--) {
+                    result[index++] = matrix[row][left];
+                }
+            }
+            top++;
+            bottom--;
+            left++;
+            right--;
         }
-
-
+        return result;
     }
 
 
